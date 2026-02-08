315860a7ad1055caacce34ce58d652111bf2e28d
sanjai-lab-dev <sanjaydhesigan@gmail.com>
2026-02-08 19:12:48 +0530

    skills added

--- app-1/src/Skills.jsx
+++ app-1/src/Skills.jsx
@@ -17,9 +17,9 @@ function Skills({ className }) {
             image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg"
         },
         {
-    name: "React",
-    image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-},
+            name: "React",
+            image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+        },
         {
             name: "Node.js",
             image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
@@ -36,11 +36,16 @@ function Skills({ className }) {
             name: "Tailwind CSS",
             image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg"
         },
-          {
+         {
+            name: "GitHub",
+            image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+        },
+        {
             name: "Postman",
-            image:"https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-plain.svg"
-
+            image: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-plain.svg"
         }
+       
+
 
     ];
 
@@ -61,27 +66,27 @@ function Skills({ className }) {
                     <Spin />
                     <Spin />
                     <Spin />
-                
+
                 </div>
             </div>
 
             {/* Foreground Content (Your Skills Title/Icons) */}
             <div className="z-10 text-white">
-                 
+
                 <div className='w-[90vw] h-[90vh] xl:w-[60vw] xl:h[90vh] border-2 border-stone-700 backdrop-blur-sm flex flex-col items-center justify-center xl:py-20 rounded-2xl'>
-                       <div className='font-extrabold text-6xl md:m-5 bg-linear-65 from-sky-500 to-yellow-500/50 p-4 bg-clip-text text-transparent underline hover:decoration-slate-600'>My Skills</div>
+                    <div className='font-extrabold text-6xl md:m-5 bg-linear-65 from-sky-500 to-yellow-500/50 p-4 bg-clip-text text-transparent underline hover:decoration-slate-600'>My Skills</div>
                     <div className='flex flex-row flex-wrap w-full justify-center'>
                         {images.map((img, index) => (
-                            <div  className=" hover:bg-gray-400/10 rounded-xl xl:p-4  animate-[start_5s_ease-out_infinite]" key={index} >
-                                <img className='w-15 h-15 p-3 m-4 md:w-20 md:h-20 lg:w-25 lg:h-25 xl:w-25 xl:h-25' src={img.image} alt="loading..." />
+                            <div className=" hover:bg-gray-300/10 rounded-xl xl:p-4  animate-[start_5s_ease-out_infinite]" key={index} >
+                                <img className='w-15 h-15 p-3 m-4 md:w-20 md:h-20 lg:w-25 lg:h-25 xl:w-25 xl:h-25 drop-shadow-xl drop-shadow-white/30' src={img.image} alt="loading..." />
                                 <h1 className='text-center font-extrabold'>{img.name}</h1>
                             </div>
                         ))}
                     </div>
 
                 </div>
-              
-            </div> 
+
+            </div>
         </div >
     );
 }
